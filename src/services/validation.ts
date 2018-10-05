ce9fe03e777726c2190e501f5d859d7e0cd61844
Mark Katerberg <katerberg@fastmail.fm>
2018-10-04 20:47:59 -0500

    Reduces mistyping

--- src/services/validation.ts
+++ src/services/validation.ts
@@ -1,9 +1,9 @@
 import { IStart, IStrategy } from '../interfaces';
 
 export const isIStart = (possible: string): boolean => {
-	return Object.values(IStart).some(s => s === possible as IStart);
+	return Object.values(IStart).some(s => s === (possible as IStart));
 };
 
 export const isIStrategy = (possible: string): boolean => {
-	return Object.values(IStrategy).some(s => s === possible as IStrategy);
+	return Object.values(IStrategy).some(s => s === (possible as IStrategy));
 };
